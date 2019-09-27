152ab600079c2b5e72f9eb58075c634f7e8825cc
diegoogle <48544269+diegoogle@users.noreply.github.com>
2019-09-27 14:42:11 -0500

    Add files via upload

---
+++ Cap08/Formas/Form1.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Formas
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btLineasRect_Click(object sender, EventArgs e)
+    {
+      Graphics g = PictureBox1.CreateGraphics();
+      Pen lápiz = new Pen(Color.Black, 3);
+      g.DrawLine(lápiz, 10, 10, 240, 100);
+
+      Rectangle rect = new Rectangle(10, 120, 230, 90);
+      g.DrawRectangle(lápiz, rect);
+    }
+
+    private void btElipsesArcos_Click(object sender, EventArgs e)
+    {
+      Graphics g = PictureBox1.CreateGraphics();
+      Pen lápiz = new Pen(Color.Black);
+      lápiz.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+      g.DrawRectangle(lápiz, 10, 10, 230, 90);
+      lápiz = new Pen(Color.Black, 3);
+      g.DrawEllipse(lápiz, 10, 10, 230, 90);
+
+      Rectangle rect = new Rectangle(10, 120, 230, 90);
+      lápiz = new Pen(Color.Black, 3);
+      lápiz.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+      g.DrawEllipse(lápiz, rect);
+      lápiz = new Pen(Color.Red, 3);
+      g.DrawArc(lápiz, rect, 30, 180);
+    }
+
+    private void btTarta_Click(object sender, EventArgs e)
+    {
+      Graphics g = PictureBox1.CreateGraphics();
+      Pen lápiz = new Pen(Color.Black, 3);
+      Rectangle rect = new Rectangle(10, 60, 230, 90);
+      g.DrawPie(lápiz, rect, 30, 150);
+    }
+
+    private void btPoligonos_Click(object sender, EventArgs e)
+    {
+      Graphics g = PictureBox1.CreateGraphics();
+      GraphicsPath trazado = new GraphicsPath();
+      Pen lápiz = new Pen(Color.Black, 3);
+
+      PointF[] triángulo = { new Point(20, 80), new Point(110, 10), new Point(230, 90) };
+      g.DrawPolygon(lápiz, triángulo);
+
+      PointF[] pentágono = { new Point(20, 150), new Point(130, 120), new Point(230, 155), new Point(190, 200), new Point(45, 195) };
+      g.DrawPolygon(lápiz, pentágono);
+    }
+
+    private void btCurvas_Click(object sender, EventArgs e)
+    {
+      // Superficie de dibujo
+      Graphics g = PictureBox1.CreateGraphics();
+
+      // Crear lápices
+      Pen lápizRojo = new Pen(Color.Red, 3);
+      Pen lápizVerde = new Pen(Color.Green, 3);
+      // Puntos que definen la curva flexible cardinal
+      Point[] puntos = { new Point(25, 25), new Point(50, 15), new Point(100, 5), new Point(120, 25), new Point(150, 50), new Point(220, 200), new Point(120, 120) };
+      // Dibujar líneas entre los puntos
+      g.DrawLines(lápizRojo, puntos);
+      // Dibujar la curva
+      g.DrawCurve(lápizVerde, puntos);
+
+      // Crear lápiz
+      Pen lápizNegro = new Pen(Color.Black, 3);
+      // Puntos que definen la curva flexible de Bézier
+      Point p1 = new Point(30, 120);
+      Point p2 = new Point(150, 200);
+      Point c1 = new Point(75, 10);
+      Point c2 = new Point(50, 210);
+      // Dibujar la curva
+      g.DrawBezier(lápizNegro, p1, c1, c2, p2);
+    }
+
+    private void btTrazados_Click(object sender, EventArgs e)
+    {
+      Graphics g = PictureBox1.CreateGraphics();
+      GraphicsPath trazado = new GraphicsPath();
+      Rectangle rect = new Rectangle(10, 10, 200, 100);
+      trazado.AddArc(rect, 45, 135);
+      trazado.AddLine(80, 100, 160, 200);
+      trazado.CloseFigure();
+      g.DrawPath(Pens.Blue, trazado);
+    }
+
+    private void btRegiones_Click(object sender, EventArgs e)
+    {
+      // Superficie de dibujo
+      Graphics g = PictureBox1.CreateGraphics();
+      // Lápiz
+      Pen lápiz = new Pen(Color.Black, 3);
+      // Centro de la superficie de dibujo
+      int xCentro = PictureBox1.Width / 2;
+      int yCentro = PictureBox1.Height / 2;
+      // Transformaciones: eje Y positivo hacia arriba y
+      // origen (0,0) en el centro
+      g.Transform = new Matrix(1, 0, 0, -1, xCentro, yCentro);
+      // Rectángulos para tres elipses
+      Rectangle rect0 = new Rectangle(-50, 0, 100, 100);
+      Rectangle rect1 = new Rectangle(-7, -75, 100, 100);
+      Rectangle rect2 = new Rectangle(-93, -75, 100, 100);
+      // Añadimos tres elipses a un trazado
+      GraphicsPath trazado = new GraphicsPath();
+      trazado.AddEllipse(rect0);
+      trazado.AddEllipse(rect1);
+      trazado.AddEllipse(rect2);
+      // Pintar el trazado
+      g.FillPath(Brushes.Yellow, trazado);
+      // Crear una región con el trazado
+      Region región = new Region(trazado);
+
+      //GraphicsPath trazado0 = new GraphicsPath();
+      //trazado0.AddEllipse(rect0);
+      //GraphicsPath trazado1 = new GraphicsPath();
+      //trazado1.AddEllipse(rect1);
+      //GraphicsPath trazado2 = new GraphicsPath();
+      //trazado2.AddEllipse(rect2);
+      //Region región = new Region(trazado0);
+      //región.Xor(trazado1);
+      //región.Xor(trazado2);
+      //g.FillRegion(Brushes.Yellow, región);
+
+      // Definir la región de recorte:
+      // región de recorte actual intersección objeto región
+      g.SetClip(región, CombineMode.Intersect);
+      // Dibujar radios desde el origen, de dos en dos grados
+      float PI = 3.1415926F;
+      float radio = Math.Min(xCentro, yCentro);
+      float a;
+      float x;
+      float y;
+      for (a = 0; a <= 2 * PI; a += PI / 90)
+      {
+        x = System.Convert.ToSingle(radio * Math.Cos(a));
+        y = System.Convert.ToSingle(radio * Math.Sin(a));
+        g.DrawLine(Pens.Red, 0, 0, x, y);
+      }
+    }
+
+    private void btLimpiar_Click(object sender, EventArgs e)
+    {
+      // Limpiar la superficie de dibujo
+      Graphics g = PictureBox1.CreateGraphics();
+      g.Clear(Color.White);
+    }
+  }
+}
---
+++ Cap08/FormasPermanentes/Form1.Designer.cs
@@ -0,0 +1,160 @@
+namespace Formas
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btLimpiar = new System.Windows.Forms.Button();
+      this.btTarta = new System.Windows.Forms.Button();
+      this.btRegiones = new System.Windows.Forms.Button();
+      this.btCurvas = new System.Windows.Forms.Button();
+      this.btPoligonos = new System.Windows.Forms.Button();
+      this.btTrazados = new System.Windows.Forms.Button();
+      this.btElipsesArcos = new System.Windows.Forms.Button();
+      this.btLineasRect = new System.Windows.Forms.Button();
+      this.PictureBox1 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // btLimpiar
+      // 
+      this.btLimpiar.Location = new System.Drawing.Point(274, 215);
+      this.btLimpiar.Name = "btLimpiar";
+      this.btLimpiar.Size = new System.Drawing.Size(75, 23);
+      this.btLimpiar.TabIndex = 17;
+      this.btLimpiar.Text = "Limpiar";
+      this.btLimpiar.Click += new System.EventHandler(this.btLimpiar_Click);
+      // 
+      // btTarta
+      // 
+      this.btTarta.Location = new System.Drawing.Point(274, 70);
+      this.btTarta.Name = "btTarta";
+      this.btTarta.Size = new System.Drawing.Size(75, 23);
+      this.btTarta.TabIndex = 16;
+      this.btTarta.Text = "Tartas";
+      this.btTarta.Click += new System.EventHandler(this.btTarta_Click);
+      // 
+      // btRegiones
+      // 
+      this.btRegiones.Location = new System.Drawing.Point(274, 186);
+      this.btRegiones.Name = "btRegiones";
+      this.btRegiones.Size = new System.Drawing.Size(75, 23);
+      this.btRegiones.TabIndex = 15;
+      this.btRegiones.Text = "Regiones";
+      this.btRegiones.Click += new System.EventHandler(this.btRegiones_Click);
+      // 
+      // btCurvas
+      // 
+      this.btCurvas.Location = new System.Drawing.Point(274, 128);
+      this.btCurvas.Name = "btCurvas";
+      this.btCurvas.Size = new System.Drawing.Size(75, 23);
+      this.btCurvas.TabIndex = 14;
+      this.btCurvas.Text = "Curvas";
+      this.btCurvas.Click += new System.EventHandler(this.btCurvas_Click);
+      // 
+      // btPoligonos
+      // 
+      this.btPoligonos.Location = new System.Drawing.Point(274, 99);
+      this.btPoligonos.Name = "btPoligonos";
+      this.btPoligonos.Size = new System.Drawing.Size(75, 23);
+      this.btPoligonos.TabIndex = 13;
+      this.btPoligonos.Text = "Polígonos";
+      this.btPoligonos.Click += new System.EventHandler(this.btPoligonos_Click);
+      // 
+      // btTrazados
+      // 
+      this.btTrazados.Location = new System.Drawing.Point(274, 157);
+      this.btTrazados.Name = "btTrazados";
+      this.btTrazados.Size = new System.Drawing.Size(75, 23);
+      this.btTrazados.TabIndex = 12;
+      this.btTrazados.Text = "Trazados";
+      this.btTrazados.Click += new System.EventHandler(this.btTrazados_Click);
+      // 
+      // btElipsesArcos
+      // 
+      this.btElipsesArcos.Location = new System.Drawing.Point(274, 41);
+      this.btElipsesArcos.Name = "btElipsesArcos";
+      this.btElipsesArcos.Size = new System.Drawing.Size(75, 23);
+      this.btElipsesArcos.TabIndex = 11;
+      this.btElipsesArcos.Text = "Elipses y ...";
+      this.btElipsesArcos.Click += new System.EventHandler(this.btElipsesArcos_Click);
+      // 
+      // btLineasRect
+      // 
+      this.btLineasRect.Location = new System.Drawing.Point(274, 12);
+      this.btLineasRect.Name = "btLineasRect";
+      this.btLineasRect.Size = new System.Drawing.Size(75, 23);
+      this.btLineasRect.TabIndex = 10;
+      this.btLineasRect.Text = "Líneas y ...";
+      this.btLineasRect.Click += new System.EventHandler(this.btLineasRect_Click);
+      // 
+      // PictureBox1
+      // 
+      this.PictureBox1.BackColor = System.Drawing.Color.White;
+      this.PictureBox1.Location = new System.Drawing.Point(13, 13);
+      this.PictureBox1.Name = "PictureBox1";
+      this.PictureBox1.Size = new System.Drawing.Size(250, 225);
+      this.PictureBox1.TabIndex = 9;
+      this.PictureBox1.TabStop = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(362, 251);
+      this.Controls.Add(this.btLimpiar);
+      this.Controls.Add(this.btTarta);
+      this.Controls.Add(this.btRegiones);
+      this.Controls.Add(this.btCurvas);
+      this.Controls.Add(this.btPoligonos);
+      this.Controls.Add(this.btTrazados);
+      this.Controls.Add(this.btElipsesArcos);
+      this.Controls.Add(this.btLineasRect);
+      this.Controls.Add(this.PictureBox1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+      this.MaximizeBox = false;
+      this.Name = "Form1";
+      this.Text = "Formas gráficas";
+      ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btLimpiar;
+    private System.Windows.Forms.Button btTarta;
+    private System.Windows.Forms.Button btRegiones;
+    private System.Windows.Forms.Button btCurvas;
+    private System.Windows.Forms.Button btPoligonos;
+    private System.Windows.Forms.Button btTrazados;
+    private System.Windows.Forms.Button btElipsesArcos;
+    private System.Windows.Forms.Button btLineasRect;
+    private System.Windows.Forms.PictureBox PictureBox1;
+  }
+}
+
