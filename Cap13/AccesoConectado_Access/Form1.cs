b9d65126eeb37417f505614e311d344b34c0c52c
diegoogle <48544269+diegoogle@users.noreply.github.com>
2019-09-27 15:41:21 -0500

    Add files via upload

---
+++ Cap13/AccesoConectado_Access/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace AccesoConectado
+{
+  public partial class Form1 : Form
+  {
+    private OleDbConnection ConexionConBD;
+    private OleDbCommand OrdenSql;
+    private OleDbDataReader Lector;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      // Crear la conexión
+      string strConexion = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+        "Data Source=C:./../../bd_telefonos.accdb;";
+      ConexionConBD = new OleDbConnection(strConexion);
+    }
+
+    private void btMostrarDatos_Click(object sender, EventArgs e)
+    {
+      using (ConexionConBD)
+      {
+        // Crear una consulta
+        string Consulta = "SELECT nombre, telefono FROM telefonos";
+        OrdenSql = new OleDbCommand(Consulta, ConexionConBD);
+        // Abrir la conexión con la base de datos
+        ConexionConBD.Open();
+        // ExecuteReader hace la consulta y devuelve un SqlDataReader
+        Lector = OrdenSql.ExecuteReader();
+        // Llamar siempre a Read antes de acceder a los datos
+        while (Lector.Read()) // siguiente registro
+        {
+          lsTfnos.Items.Add(Lector["nombre"] + " " +
+                            Lector["telefono"]);
+        }
+        // Llamar siempre a Close una vez finalizada la lectura
+        Lector.Close();
+      }
+      //btMostrarDatos.Enabled = false;
+    }
+  }
+}
