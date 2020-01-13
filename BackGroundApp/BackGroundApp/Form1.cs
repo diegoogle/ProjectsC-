8fb60bc456e809f4800994c90b83128c17ceacd7
diegoogle <48544269+diegoogle@users.noreply.github.com>
2020-01-13 09:57:38 -0600

    Add files via upload

---
+++ BackGroundApp/BackGroundApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BackGroundApp {
+    public partial class Form1: Form {
+        public Form1 () {
+            InitializeComponent();
+        }
+
+        private void button1_Click (object sender, EventArgs e) {
+            if (backgroundWorker1.IsBusy != true) {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork (object sender, DoWorkEventArgs e) {
+            for (int i =1;i<=10;i++) {
+                if (backgroundWorker1.CancellationPending == true) {
+                    e.Cancel = true;
+                    break;
+                }
+                Thread.Sleep(1000);
+                backgroundWorker1.ReportProgress(i * 10);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged (object sender, ProgressChangedEventArgs e) {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted (object sender, RunWorkerCompletedEventArgs e) {
+            MessageBox.Show("Proceso terminado");
+        }
+
+        private void button2_Click (object sender, EventArgs e) {
+            backgroundWorker1.CancelAsync();
+        }
+    }
+}
