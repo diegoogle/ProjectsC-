8e5d4b0c73ba2199057d5fc5ae4d2a0df474d947
diegoogle <48544269+diegoogle@users.noreply.github.com>
2020-03-04 16:45:03 -0600

    Add files via upload

---
+++ Monitor/Monitor/Log.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace Monitor {
+    public class Log {
+        private string path = "";        
+        public Log(string path) {
+            this.path = path;
+        }
+        public void Add(string sLog) {
+            CreateDirectory();
+            string nombre = GetNameFile();
+            string cadena = "";
+            cadena += DateTime.Now + " - " + sLog + Environment.NewLine;
+
+            StreamWriter sw = new StreamWriter(path + "/" + nombre, true);//True es para anexar linea nueva
+            sw.Write(cadena);
+            sw.Close();
+        }
+        #region HELPER
+        private string GetNameFile() {
+            string nombre = "";
+            nombre = "log_" + DateTime.Now.Year + "_" + DateTime.Now.Month + "_" + DateTime.Now.Day + ".log";
+            return nombre;
+        }
+        private void CreateDirectory() {
+            try {
+                if (Directory.Exists(path))//Comprueba si existe el directorio
+                    Directory.CreateDirectory(path);
+            } catch (DirectoryNotFoundException e) {
+                throw new Exception(e.Message);
+            }            
+        }
+        #endregion
+    }
+}
---
+++ Monitor/Monitor/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Monitor {
+    class Program {
+        static void Main(string[] args) {
+            Log log = new Log(@"C:\Release\");
+            log.Add("Hola Mundo otra vez");
+        }
+    }
+}
